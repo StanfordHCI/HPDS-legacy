8e757b8a2f816ce854be62283f469f33a532845a
cooper-mj <coopermj@stanford.edu>
2018-08-31 09:59:47 -0700

    Added Python framework to read in building data, added sample building data

---
+++ Data-Aggregation/read_building_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+'''
+Reads data from a .csv file containing building data (view sample
+in this directory) into a pandas dataframe for analysis.
+'''
+def read_building_data(filename):
+	# header parameter determines which row of the .csv is to be used
+	# as the headers for the dataframe. As you can see from the sample
+	# data, that is row 1; other building data may not conform to this
+	# format, and so the header parameter may need to be adjusted
+	# accordingly.
+	df = pd.read_csv(filename, header=1)
+	return df
+
+
+if __name__ == "__main__":
+	print(read_building_data("sample_buildingdata.csv"))
