0c1271a953b75fed5cc2a5f7051e417a64661296
Joshua Ren <jr@Joshuas-MacBook-Pro-2.local>
2019-07-17 15:28:15 -0700

    update

--- Data-Aggregation/analysis.py
+++ Data-Aggregation/analysis.py
@@ -6,20 +6,21 @@ import matplotlib.pyplot as plt
 
 # analyze plugin_ios_activity_recognition data from AWARE server SQL database.
 def activitiesAnalysis(df):
-    plt.close('all')
-    gdf = df.groupby("activities")  # group by activities type for aggregate
-    actMap = {}
-    # create a map that links activities type to frequency
-    for key, item in gdf:
-        actMap[key] = len(gdf.get_group(key))
-    labels = list(actMap.keys())
-    sizes = list(actMap.values())
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-            startangle=90)
-    # Equal aspect ratio ensures that pie is drawn as a circle.
-    ax1.axis('equal')
-    plt.show()
+    print(df)
+    # plt.close('all')
+    # gdf = df.groupby("activities")  # group by activities type for aggregate
+    # actMap = {}
+    # # create a map that links activities type to frequency
+    # for key, item in gdf:
+    #     actMap[key] = len(gdf.get_group(key))
+    # labels = list(actMap.keys())
+    # sizes = list(actMap.values())
+    # fig1, ax1 = plt.subplots()
+    # ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+    #         startangle=90)
+    # # Equal aspect ratio ensures that pie is drawn as a circle.
+    # ax1.axis('equal')
+    # plt.show()
 
 
 # analyze healthkit quantity data from AWARE server SQL database.
@@ -28,6 +29,11 @@ def healthQuanAnalysis(df):
     # displays the entries of each HealthKit type grouped by type
     for key, item in hdf:
         print(hdf.get_group(key), "\n\n")
+
+
+# analyze healthkit category data from AWARE server SQL database.
+def healthCatAnalysis(df):
+    print(df)
 
 
 # analyze qualtrics survey data
@@ -47,8 +53,11 @@ if __name__ == "__main__":
                                    password, database_name, "plugin_ios_activity_recognition")
     healquandf = gen_df_from_remote_SQL(hostname, username,
                                         password, database_name, "health_kit_quantity")
+    # healcatdf = gen_df_from_remote_SQL(hostname, username,
+    # password, database_name, "health_kit_category")
     qualdf = read_csv_data('yah yeet_July 8, 2019_16.24.csv')
 
     # qualtricsAnalysis(qualdf)
     activitiesAnalysis(actdf)
-    # healthQuanAnalysis(healquandf)
+    healthQuanAnalysis(healquandf)
+    # healthCatAnalysis(healcatdf)
